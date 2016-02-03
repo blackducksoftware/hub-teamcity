6d743c30ddf2086653151e99ac47bed099da2207
jrichard <jrichard@localhost>
2016-02-03 11:09:46 -0500

    ITH-47 Fixing the logging to be the same as the Jenkins plugin. We split the CLI output and print the verbose logs to a file in the workspace and only the interesting logs to the Build Output

--- agent/src/main/java/com/blackducksoftware/integration/hub/teamcity/agent/scan/TeamCityScanExecutor.java
+++ agent/src/main/java/com/blackducksoftware/integration/hub/teamcity/agent/scan/TeamCityScanExecutor.java
@@ -4,6 +4,7 @@ import static java.lang.ProcessBuilder.Redirect.PIPE;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.blackducksoftware.integration.hub.ScanExecutor;
+import com.blackducksoftware.integration.hub.ScannerSplitStream;
 import com.blackducksoftware.integration.hub.exception.HubIntegrationException;
 
 public class TeamCityScanExecutor extends ScanExecutor {
@@ -77,6 +79,11 @@ public class TeamCityScanExecutor extends ScanExecutor {
     protected Result executeScan(List<String> cmd, String logDirectoryPath) throws HubIntegrationException, InterruptedException {
         try {
 
+            File logBaseDirectory = new File(getLogDirectoryPath());
+            logBaseDirectory.mkdirs();
+            File standardOutFile = new File(logBaseDirectory, "CLI_Output.txt");
+            standardOutFile.createNewFile();
+
             // ////////////////////// Code to mask the password in the logs
             List<String> cmdToOutput = new ArrayList<String>();
             cmdToOutput.addAll(cmd);
@@ -103,14 +110,35 @@ public class TeamCityScanExecutor extends ScanExecutor {
 
             // Should use the split stream for the process
 
-            Process hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).redirectOutput(PIPE).start();
-            int returnCode = hubCliProcess.waitFor();
+            FileOutputStream outputFileStream = new FileOutputStream(standardOutFile);
 
-            String outputString = readStream(hubCliProcess.getInputStream());
-            outputString = outputString + System.getProperty("line.separator") + readStream(hubCliProcess.getErrorStream());
+            String outputString = "";
+            ScannerSplitStream splitOutputStream = new ScannerSplitStream(getLogger(), outputFileStream);
+
+            Process hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).redirectOutput(PIPE).start();
+
+            // The Cli logs go the error stream for some reason
+            StreamRedirectThread redirectThread = new StreamRedirectThread(hubCliProcess.getErrorStream(), splitOutputStream);
+            redirectThread.start();
+
+            int returnCode = hubCliProcess.waitFor();
+            redirectThread.join();
+
+            splitOutputStream.flush();
+            splitOutputStream.close();
+
+            if (splitOutputStream.hasOutput()) {
+                outputString = splitOutputStream.getOutput();
+            }
+            outputString = outputString + " \n" + readStream(hubCliProcess.getInputStream());
 
             if (outputString.contains("Illegal character in path")
                     && (outputString.contains("Finished in") && outputString.contains("with status FAILURE"))) {
+                standardOutFile.delete();
+                standardOutFile.createNewFile();
+
+                splitOutputStream = new ScannerSplitStream(getLogger(), outputFileStream);
+
                 // This version of the CLI can not handle spaces in the log directory
                 // Not sure which version of the CLI this issue was fixed
 
@@ -121,13 +149,27 @@ public class TeamCityScanExecutor extends ScanExecutor {
                 cmd.remove(indexOfLogOption);
                 cmd.add(indexOfLogOption, logPath);
 
-                hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).start();
-                returnCode = hubCliProcess.waitFor();
+                hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).redirectOutput(PIPE).start();
+                // The Cli logs go the error stream for some reason
+                redirectThread = new StreamRedirectThread(hubCliProcess.getErrorStream(), splitOutputStream);
+                redirectThread.start();
 
-                outputString = readStream(hubCliProcess.getInputStream());
-                outputString = outputString + System.getProperty("line.separator") + readStream(hubCliProcess.getErrorStream());
+                returnCode = hubCliProcess.waitFor();
+                redirectThread.join();
+
+                splitOutputStream.flush();
+                splitOutputStream.close();
+
+                if (splitOutputStream.hasOutput()) {
+                    outputString = splitOutputStream.getOutput();
+                }
+                outputString = outputString + " \n" + readStream(hubCliProcess.getInputStream());
             } else if (outputString.contains("Illegal character in opaque")
                     && (outputString.contains("Finished in") && outputString.contains("with status FAILURE"))) {
+                standardOutFile.delete();
+                standardOutFile.createNewFile();
+
+                splitOutputStream = new ScannerSplitStream(getLogger(), outputFileStream);
 
                 int indexOfLogOption = cmd.indexOf("--logDir") + 1;
 
@@ -139,15 +181,24 @@ public class TeamCityScanExecutor extends ScanExecutor {
                 cmd.remove(indexOfLogOption);
                 cmd.add(indexOfLogOption, logPath);
 
-                hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).start();
-                returnCode = hubCliProcess.waitFor();
+                hubCliProcess = new ProcessBuilder(cmd).redirectError(PIPE).redirectOutput(PIPE).start();
+                // The Cli logs go the error stream for some reason
+                redirectThread = new StreamRedirectThread(hubCliProcess.getErrorStream(), splitOutputStream);
+                redirectThread.start();
 
-                outputString = readStream(hubCliProcess.getInputStream());
-                outputString = outputString + System.getProperty("line.separator") + readStream(hubCliProcess.getErrorStream());
+                returnCode = hubCliProcess.waitFor();
+                redirectThread.join();
+
+                splitOutputStream.flush();
+                splitOutputStream.close();
+
+                if (splitOutputStream.hasOutput()) {
+                    outputString = splitOutputStream.getOutput();
+                }
+                outputString = outputString + " \n" + readStream(hubCliProcess.getInputStream());
             }
 
             getLogger().info("Hub CLI return code : " + returnCode);
-            getLogger().info(outputString);
 
             if (logDirectoryPath != null) {
                 File logDirectory = new File(logDirectoryPath);
